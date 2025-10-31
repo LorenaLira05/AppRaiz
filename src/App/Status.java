283865ad49c9257c7a65bf366a5c95ffb7a08935
Lorena Lira <naheelee2005@gmail.com>
2025-10-31 09:22:30 -0300

    Refactor Status class for consistency and formatting

--- src/App/Status.java
+++ src/App/Status.java
@@ -1,29 +1,31 @@
 package App;
 
 public class Status {
-		private int id;
-		private String status;
+	private int id;
+	private String status;
 	 
-		public Status() {
-		 }
-		public Status(int id, String status) {
-			this.id = id;
-			this.status = status;
-			}
+	public Status() {
+		}
+	
+	public Status(int id, String status) {
+		this.id = id;
+		this.status = status;
+		}
 		
-		public int getId() {
-			return this.id;
-			}
-			public void setId(int id) {
-			this.id = id;
-			}
+	public int getId() {
+		return this.id;
+		}
+	
+	public void setId(int id) {
+		this.id = id;
+		}
 			
-			public String getStatus() {
-				return this.status;
-			}
+	public String getStatus() {
+		return this.status;
+		}
 			
-				public void setStatus (String status) {
-				this.status = status;
-			}
+	public void setStatus (String status) {
+		this.status = status;
+		}
 
 	}
